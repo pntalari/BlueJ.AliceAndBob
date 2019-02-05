69dc56757d8824c75895f8da23c066e3cc976ca3
Nirmala Talari <nirmalatalari@zipcoders-MacBook-Pro.local>
2019-02-05 13:49:18 -0500

    Alice&Bob Greeting Labwork

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args )
+    {
+     Scanner read = new Scanner(System.in);
+     System.out.println("Enter your Kool name: ");
+     String input = read.next();
+     
+     if (input.equals("Alice") || input.equals("Bob"))
+       System.out.println("Hello " + input + ".Welcome to my world!");
+     else
+        System.out.println("I dont know whoever you are, Sorry!");
 
     }
 }
